56c01bc28f4cf4d264424382ab6b1212d65c9239
Léo <leonardo.soares1@mail.uft.edu.br>
2026-03-02 19:40:08 -0300

    Reorganização das pastas e adição de um exercício da revisão de ED-I

---
+++ Pessoais/src/main/java/Exercicios/ED_I/EX1.java
@@ -0,0 +1,30 @@
+package Exercicios.ED_I;
+
+import java.util.Scanner;
+
+public class EX1 {
+
+    static final int MAX = 10;
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] V = new int[MAX];
+        int x;
+        for (int i = 0; i < MAX; i++) {
+            V[i] = scanner.nextInt();
+        }
+
+        x = scanner.nextInt();
+
+        for (int i = 0; i < MAX; i++) {
+            if (V[i] % x == 0) {
+                System.out.print(V[i] + " ");
+            }
+        }
+    }
+}
+
+
+
+
