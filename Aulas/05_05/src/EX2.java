bbdb41b0ce5d81f032846dd816e03e77b7076314
leouft <leonardo.soares1@mail.uft.edu.br>
2026-05-05 10:53:57 -0300

    exercícios do dia 05 do 05

---
+++ Aulas/05_05/src/EX2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class EX2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int N;
+
+        System.out.print("Quantos números você vai digitar? ");
+        N = sc.nextInt();
+
+        double[] V = new double[N];
+
+        double soma = 0;
+
+        for(int i = 0; i < N; i++) {
+            System.out.print("Digite o número: ");
+            V[i] = sc.nextDouble();
+            soma += V[i];
+        }
+
+        System.out.print("\nVALORES = ");
+        for(int i = 0; i < N; i++) {
+            System.out.print(V[i] + " ");
+        }
+        System.out.println("\nSOMA = " + soma);
+        System.out.println("MEDIA = " + soma/N);
+    }
+}
